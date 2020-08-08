295b18c2535c262997a244db706f7732ef868fc7
elnazer-abdalmnm <69108345+elnazer-abdalmnm@users.noreply.github.com>
2020-08-08 08:56:39 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('arrow').onclick=function(){
+document.getElementById("part").classList.toggle("show")
+document.getElementById("arrow").classList.toggle("bagr")
+document.getElementById("myhope").classList.toggle("new")
+
+}
+ 
